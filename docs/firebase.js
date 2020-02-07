5a91be3ede66d23eefc67363b1d7323d51941f5f
shtnkgm <nakagami.shota@gmail.com>
2020-02-07 23:57:22 +0900

    add firebase

---
+++ docs/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDziLrTTCVsOHm3chF42tOjm88sA8I-a9s",
+    authDomain: "swiftcodegenerator.firebaseapp.com",
+    databaseURL: "https://swiftcodegenerator.firebaseio.com",
+    projectId: "swiftcodegenerator",
+    storageBucket: "swiftcodegenerator.appspot.com",
+    messagingSenderId: "298498546493",
+    appId: "1:298498546493:web:e2ad46b5b5cbffafdfc354",
+    measurementId: "G-G0SXMZDZMR"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
