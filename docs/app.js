7a079692c810ae429a14e9cea5325ddc213b9e05
shtnkgm <nakagami.shota@gmail.com>
2020-02-10 01:07:23 +0900

    refactor

--- docs/app.js
+++ docs/app.js
@@ -1,15 +1,112 @@
 class Type {
-  constructor(name, properties) {
-    this.name = name;
-    this.properties = properties;
+  constructor(rawText) {
+    this.rawText = rawText;
+    this.typeName = Type.extractTypeName(rawText);
+    this.storedProperties = Type.extractStoredProperties(rawText);
+  }
+
+  static extractTypeName(rawText) {
+    const output = rawText
+      .split("\n")
+      .filter(line => line.includes('class') || line.includes('struct'))
+      .join("")
+      .replace(/.*(class|struct) ([a-zA-Z0-9]+).*/g, '$2')
+    return output.length == 0 ? "<#FixMe#>" : output
+  }
+
+  static extractStoredProperties(rawText) {
+    return rawText
+      .split("\n")
+      .filter(line => !line.includes('class') || !line.includes('struct')) // remove type definition
+      .filter(line => !line.includes('{')) // remove computed property
+      .filter(line => !(line.includes('let') && line.includes('='))) // remove let with default value
+      .filter(line => line.includes('var') || line.includes('let')) // remove lines without let or var
+      .map(function (line) {
+        const property = new StoredProperty(line)
+        return property
+      });
   }
 }
 
-class Property {
-  constructor(name, typeName, defaultValue) {
-    this.name = name;
-    this.typeName = typeName;
-    this.defaultValue = defaultValue;
+class StoredProperty {
+  constructor(rawText) {
+    let removedText = rawText
+      .replace(/\/\/.*/g, '') // remove comment
+      .replace(/@[a-zA-Z0-9]* /g, '') // remove attribute
+      .replace(/(open|public|internal|fileprivate|private) /g, '') // remove access control
+      .replace(/(let|var) /g, '') // remove let var
+      .trim()
+
+    this.propertyName = StoredProperty.extractPropertyName(removedText);
+    this.typeName = StoredProperty.extractTypeName(removedText);
+  }
+
+  static extractPropertyName(rawText) {
+    const value = rawText.match(/^([a-zA-Z0-9]*)/)
+
+    if (value) {
+      return value[1]
+    }
+    return "<#FixMe#>"
+  }
+
+  static extractTypeName(rawText) {
+    switch (true) {
+      case /= (false|true)$/.test(rawText):
+        return 'Bool'
+
+      case /= ".*"$/.test(rawText):
+        return 'String'
+    }
+
+    const value = rawText.match(/: ([a-zA-Z0-9\.\?\]\[: ]*)/)
+    if (value) {
+      return value[1].trim()
+    }
+    return "<#FixMe#>"
+  }
+
+  get defaultValue() {
+    switch (true) {
+      case /^String$/.test(this.typeName):
+        return '""'
+
+      case /^(Int(|8|16|32|64)|Float|Double|CGFloat)$/.test(this.typeName):
+        return '0'
+
+      case /^(CGPoint|CGRect|CGSize)$/.test(this.typeName):
+        return '.zero'
+
+      case /^Decimal$/:
+        return 'Decimal()'
+
+      case /^URL$/.test(this.typeName):
+        return 'URL(string: "https://sample.com")!'
+
+      case /^Date$/.test(this.typeName):
+        return 'Date()'
+
+      case /^Data$/.test(this.typeName):
+        return 'Data()'
+
+      case /^Bool$/.test(this.typeName):
+        return 'false'
+
+      case /^.*\?$/.test(this.typeName): // Optional
+        return 'nil'
+
+      case /^\[.*: .*\]$/.test(this.typeName): // Disctionary
+        return '[:]'
+
+      case /^\[.*\]$/.test(this.typeName): // Collection
+        return '[]'
+
+      case /^<#FixMe#>$/.test(this.typeName):
+        return '<#FixMe#>'
+
+      default:
+        return '.make()'
+    }
   }
 }
 
@@ -48,6 +145,7 @@ struct Book {
     let defaultLet: Int = 1
     var noTypeAnotation = 1
     var noTypeAnotationString = "hoge"
+    var noTypeAnotationBool = true
 }`
   },
   // mounted: function () {
@@ -56,14 +154,6 @@ struct Book {
   //   })
   // },
   computed: {
-    typeName() {
-      output = this.text
-        .split("\n")
-        .filter(line => line.includes('class') || line.includes('struct'))
-        .join("")
-        .replace(/.*(class|struct) ([a-z|A-Z|0-9]+).*/g, '$2')
-      return output.length == 0 ? "<#FixMe#>" : output
-    },
     storedPropertyLines() {
       return this.text
         .split("\n")
@@ -73,34 +163,19 @@ struct Book {
         .filter(line => line.includes('var') || line.includes('let')) // remove lines without let or var
         .join("\n")
     },
-    normalizedText() {
+    properties() {
       return this.storedPropertyLines
         .split("\n")
         .map(function (line) {
-          line
-          // .replace(/\/\/.*/g, '') // remove comment
-          // .replace(/@[a-z|A-Z|0-9]* /g, '') // remove attribute
-          // .replace(/(open|public|internal|fileprivate|private) /g, '') // remove access control
-          // .replace(/(let|var) /g, '') // remove let var
-          // .replace(/([a-z|A-Z|0-9]*) = "(.*)"$/g, '$1: String = $2')
-          // .trim()
-        })
-        .join("\n")
-    },
-    properties() {
-      return this.normalizedText
-        .split("\n")
-        .map(function (line) {
-          const propertyName = line.match(/^([a-z|A-Z|0-9]*):/)[1]
-          const propertyTypeName = line.match(/: ([a-z|A-Z|0-9]*)/)[1]
-          const propertyDefaultValue = line.match(/= ([a-z|A-Z|0-9]*)$/)[1]
+          const propertyName = line.match(/^([a-zA-Z0-9]*):/)[1]
+          const propertyTypeName = line.match(/: ([a-zA-Z0-9]*)/)[1]
+          const propertyDefaultValue = line.match(/= ([a-zA-Z0-9]*)$/)[1]
           const property = new Property(propertyName, propertyTypeName, propertyDefaultValue)
           return property
         });
     },
     type() {
-      const type = new Type(this.typeName, this.properties)
-      return type
+      return new Type(this.text)
     }
   },
   methods: {
@@ -123,71 +198,24 @@ struct Book {
         .filter(line => !(line.includes('let') && line.includes('='))) // remove let with default value
         .filter(line => line.includes('var') || line.includes('let')) // remove lines without let or var
         .map(line => line.replace(/\/\/.*/g, '')) // remove comment
-        .map(line => line.replace(/@[a-z|A-Z|0-9]* /g, '')) // remove attribute
+        .map(line => line.replace(/@[a-zA-Z0-9]* /g, '')) // remove attribute
         .map(line => line.replace(/(open|public|internal|fileprivate|private) /g,
           '')) // remove access control
         .map(line => line.replace(/(let|var) /g, '')) // remove let var
         .map(line => line.trim())
         .join("\n")
     },
-    factoryMethod: function (text, typeName) {
+    memberwiseInitializer: function (text, type) {
       if (text.length == 0) {
         return "\n"
       }
-      lines = text.split("\n")
-      parameters = lines
-        .map(line => line
-          .replace(/:(| )Bool$/g, ': Bool = false') // Bool
-          .replace(/:(| )((U|)Int(|8|16|32|64)|Float|Double)$/g, ': $2 = 0') // Int
-          .replace(/:(| )String$/g, ': String = ""') // String
-          .replace(/:(| )URL$/g, ': URL = URL(string: "https://sample.com")!') // URL
-          .replace(/\[([a-zA-Z0-9\.]*): ([a-zA-Z0-9\.]*)\]$/g, '[$1: $2] = [:]') // Dictionary
-          .replace(/\[([a-zA-Z0-9\.]*)\]$/g, '[$1] = []') // Collection
-          .replace(/(: .*\?).*/g, '$1 = nil') // Optional
-          .replace(/: ([a-zA-Z0-9\.]*$)/g, ': $1 = .make()') // custom type
-          .replace(/^([a-zA-Z0-9\.]*) = "/g, '$1: String = "') // no type anotation string
-          .replace(/^([a-zA-Z0-9\.]*) =/g, '$1: <#FixMe#> =') // no type anotation
-          .trim()
-        )
+      parameters = type.storedProperties
+        .map(line => `${line.propertyName}: ${line.typeName}`)
         .join(',\n        ')
-      body = lines
-        .map(line => line
-          .replace(/:.*/g, '')
-          .replace(/ =.*/g, '')
-        )
-        .map(line => line + ": " + line)
-        .join(",\n            ")
-      return `extension ${typeName} {
-    static func make(
-        ${parameters}
-    ) -> ${typeName} {
-        return ${typeName}(
-            ${body}
-        )
-    }
-}`
-    },
-    memberwiseInitializer: function (text, typeName) {
-      if (text.length == 0) {
-        return "\n"
-      }
-      lines = text.split("\n")
-      parameters = lines
-        .map(line => line
-          .replace(/^([a-zA-Z0-9\.]*) = ".*/g, '$1: String') // no type anotation string
-          .replace(/ =.*/g, '') // remove default value
-          .replace(/^([a-zA-Z0-9\.]*$)/g, '$1: <#FixMe#>') // no type anotation
-          .trim()
-        )
-        .join(',\n        ')
-      body = lines
-        .map(line => line
-          .replace(/:.*/g, '')
-          .replace(/ =.*/g, '')
-        )
-        .map(line => "self." + line + " = " + line)
+      body = type.storedProperties
+        .map(line => `self.${line.propertyName} = ${line.propertyName}`)
         .join("\n        ")
-      return `extension ${typeName} {
+      return `extension ${type.typeName} {
     init(
         ${parameters}
     ) {
@@ -195,94 +223,75 @@ struct Book {
     }
 }`
     },
-    decodableInitializer: function (text, typeName) {
+    factoryMethod: function (text, type) {
       if (text.length == 0) {
         return "\n"
       }
-      lines = text.split("\n")
-      body = lines
-        .map(line => line
-          .replace(/^([a-zA-Z0-9\.]*) = ".*/g, '$1: String')
-          .replace(/ =.*/g, '') // remove default value
-          .replace(/^([a-zA-Z0-9\.]*$)/g, '$1: <#FixMe#>') // no type anotation
-          .replace(/^([a-zA-Z0-9\.]*): \[([a-zA-Z0-9\.]*).*: ([a-zA-Z0-9\.]*).*\]$/g, '$1 = try values.decode(<#FixMe#>.self, forKey: .$1)') // Dictionary
-          .replace(/^([a-zA-Z0-9\.]*): \[([a-zA-Z0-9\.]*).*\]$/g, '$1 = try values.decode([$2].self, forKey: .$1)') // Collection
-          .replace(/^([a-zA-Z0-9\.]*): ([a-zA-Z0-9\.]*).*$/g, '$1 = try values.decode($2.self, forKey: .$1)')
+      parameters = type.storedProperties
+        .map(line => `${line.propertyName}: ${line.typeName} = ${line.defaultValue}`)
+        .join(',\n        ')
+      body = type.storedProperties
+        .map(line => `${line.propertyName}: ${line.propertyName}`)
+        .join(",\n            ")
+      return `extension ${type.typeName} {
+    static func make(
+        ${parameters}
+    ) -> ${type.typeName} {
+        return ${type.typeName}(
+            ${body}
         )
+    }
+}`
+    },
+    decodableInitializer: function (text, type) {
+      if (text.length == 0) {
+        return "\n"
+      }
+      body = type.storedProperties
+        .map(line => `${line.propertyName} = try container.decode(${line.typeName}.self, forKey: .${line.propertyName})`)
         .join("\n        ")
-      return `extension ${typeName}: Decodable {
+      return `extension ${type.typeName}: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         ${body}
     }
 }`
     },
-    encodableImplementation: function (text, typeName) {
+    encodableImplementation: function (text, type) {
       if (text.length == 0) {
         return "\n"
       }
       lines = text.split("\n")
-      body = lines
-        .map(line => line
-          .replace(/ =.*/g, '') // remove default value
-          .replace(/:.*/g, '') // remove type
-          .replace(/^([a-zA-Z0-9\.]*)$/g, 'try container.encode($1, forKey: .$1)')
-        )
+      body = type.storedProperties
+        .map(line => `try container.encode(${line.propertyName}, forKey: .${line.propertyName})`)
         .join("\n        ")
-      return `extension ${typeName}: Encodable {
+      return `extension ${type.typeName}: Encodable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         ${body}
     }
 }`
     },
-    equatable: function (text, typeName) {
+    equatable: function (text, type) {
       if (text.length == 0) {
         return "\n"
       }
-      lines = text.split("\n")
-      parameters = lines
-        .map(line => line
-          .replace(/ =.*/g, '') // remove default value
-          .replace(/^([a-zA-Z0-9\.]*$)/g, '$1: <#FixMe#>') // no type anotation
-          .trim()
-        )
-        .join(',\n        ')
-      body = lines
-        .map(line => line
-          .replace(/:.*/g, '')
-          .replace(/ =.*/g, '')
-        )
-        .map(line => "lhs." + line + " == " + "rhs." + line)
+      body = type.storedProperties
+        .map(line => `lhs.${line.propertyName} == rhs.${line.propertyName}`)
         .join(" &&\n            ")
-      return `extension ${typeName}: Equatable {
-    static func == (lhs: ${typeName}, rhs: ${typeName}) -> Bool {
+      return `extension ${type.typeName}: Equatable {
+    static func == (lhs: ${type.typeName}, rhs: ${type.typeName}) -> Bool {
         return
             ${body}
     }
 }`
     },
-    matchable: function (text, typeName) {
+    matchable: function (text, type) {
       if (text.length == 0) {
         return "\n"
       }
-      lines = text.split("\n")
-      parameters = lines
-        .map(line => line
-          .replace(/ =.*/g, '') // remove default value
-          .replace(/^([a-zA-Z0-9\.]*$)/g, '$1: <#FixMe#>') // no type anotation
-          .trim()
-        )
-        .join(',\n        ')
-      body = lines
-        .map(line => line
-          .replace(/:.*/g, '')
-          .replace(/ =.*/g, '')
-        )
-        .map(line => "lhs." + line + " == " + "rhs." + line)
-        .join(" &&\n            ")
-      return `extension ${typeName}: Matchable {
-    public var matcher: ParameterMatcher<${typeName}> {
+      return `extension ${type.typeName}: Matchable {
+    public var matcher: ParameterMatcher<${type.typeName}> {
         return equal(to: self)
     }
 }`
